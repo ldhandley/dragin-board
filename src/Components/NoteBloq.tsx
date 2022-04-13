da9899aaba9129995b2c514c6a6c6dbd3a0c4726
Eric Stevens <Eric@learningdesignnetwork.com>
2022-04-13 15:12:46 -0700

    Mulligan

---
+++ src/Components/NoteBloq.tsx
@@ -0,0 +1,10 @@
+import Draggable from 'react-draggable';
+import '../renderer/App.css';
+
+export default function NoteBloq() {
+  return (
+    <Draggable>
+      <div className="box">note title will go here</div>
+    </Draggable>
+  )
+}
