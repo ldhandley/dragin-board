05a1edb11ebc5b27b42e0d02bfbb28f06ca1dc1c
Eric Stevens <Eric@learningdesignnetwork.com>
2022-04-13 17:20:30 -0700

    PrettierLint test.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,16 @@
 module.exports = {
   extends: 'erb',
+  extends: 'prettier',
+  plugins: 'prettier',
+
+  
   rules: {
     // A temporary hack related to IDE not resolving correct package.json
     'import/no-extraneous-dependencies': 'off',
     'import/no-unresolved': 'error',
     // Since React 17 and typescript 4.1 you can safely disable the rule
     'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': ['error']
   },
   parserOptions: {
     ecmaVersion: 2020,
