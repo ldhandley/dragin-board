d8f57b0d13e2f5d0fe1dd7e47c1907e4f74ec50e
Eric Stevens <Eric@learningdesignnetwork.com>
2022-04-16 16:58:50 -0700

    Simple whiteboard added.

--- src/Components/NoteBloq.tsx
+++ src/Components/NoteBloq.tsx
@@ -4,13 +4,11 @@ const bootstrap = require('bootstrap');
 
 export default function NoteBloq() {
   return (
-    <Draggable>
+    <Draggable bounds="parent">
       <div className="card">
-        <div className="card-header">
-           Bootstrap Card
-        </div>
+        <div className="card-header">Experience</div>
         <div className="card-body">
-          <h5 className="card-title">Title information</h5>
+          <h6 className="card-subtitle mb-2 text muted">01-26-22 - 01-30-22</h6>
           <p className="card-text">We can put text in here.</p>
         </div>
       </div>
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,6 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
