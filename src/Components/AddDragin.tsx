2a6d6dd12282336d4f3befa86007208e64186c83
trithir <ericstevens10@gmail.com>
2022-04-19 19:15:10 -0700

    attempting to implement props.

--- src/Components/AddDragin.tsx
+++ src/Components/AddDragin.tsx
@@ -1,15 +1,35 @@
 import NoteBloq from './NoteBloq';
+import React from 'react';
 
-export default function AddDragin(props: any) {
-  const createNoteBloq = () => {
-    props.setDraginList(props.draginList.concat(<NoteBloq />));
+interface draginInterface {
+  draginProps: {
+    draginList: { rendered: any };
+    setDraginList: { rendered: any };
   };
+}
 
-  return (
-    <div className="buttonMenu">
-      <button type="button" className="btn btn-info" onClick={createNoteBloq}>
-        Info
-      </button>
-    </div>
-  );
+export default class AddDragin extends React.Component<draginInterface> {
+  render() {
+    const {
+      draginProps: { draginList, setDraginList },
+    } = this.props;
+    if (draginList != undefined) {
+    {
+      const createNoteBloq = () => {
+        setDraginList.rendered(draginList.rendered.concat(<NoteBloq />));
+      };
+
+      return (
+        <div className="buttonMenu">
+          <button
+            type="button"
+            className="btn btn-info"
+            onClick={createNoteBloq}
+          >
+            AddDragin
+          </button>
+        </div>
+      );
+    }
+  }}
 }
